6ad582d8cc2c2ffc249b3dfd40fc2714fcd661c3
Damjan Pavlica <mudroljub@gmail.com>
2021-03-19 20:04:24 +0100

    motor

--- 95-motor-auto/95-motor-auto.ino
+++ 95-motor-auto/95-motor-auto.ino
@@ -4,12 +4,16 @@ int motor1levi  = 9;  // Pin 14 of L293
 int motor1desni  = 10;  // Pin 10 of L293
 int motor2levi  = 6; // Pin  7 of L293
 int motor2desni  = 5;  // Pin  2 of L293
+int echoPin = 7;
+int trigPin = 8;
 
 void setup(){
     pinMode(motor1levi, OUTPUT);
     pinMode(motor1desni, OUTPUT);
     pinMode(motor2levi, OUTPUT);
     pinMode(motor2desni, OUTPUT);
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
 }
 
 void ideNapred(int speed) {
@@ -20,5 +24,19 @@ void ideNapred(int speed) {
 }
 
 void loop(){
-  ideNapred(100);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH); // da ode i da se vrati
+  long cm = (duration/2) / 29.1;
+
+  if (cm > 20) {
+    ideNapred(100);
+  } else {
+    ideNapred(0);
+  }
+
 }
