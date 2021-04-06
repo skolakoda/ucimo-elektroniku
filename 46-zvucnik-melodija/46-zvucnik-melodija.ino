c06529fcfb818832e009b8c50076f628fd1ac2b6
Damjan Pavlica <mudroljub@gmail.com>
2021-04-06 09:12:48 +0200

    m

--- 46-zvucnik-melodija/46-zvucnik-melodija.ino
+++ 46-zvucnik-melodija/46-zvucnik-melodija.ino
@@ -1,27 +1,22 @@
 // http://www.arduino.cc/en/Tutorial/Tone
 #include "pitches.h"
 
-// notes in the melody:
 int melody[] = {NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4};
-
-// 4 = quarter note, 8 = eighth note, etc.
-int noteDurations[] = {4, 8, 8, 4, 4, 4, 4, 4};
+int noteDurations[] = {4, 8, 8, 4, 4, 4, 4, 4}; // 4: quarter note, 8: eighth note
 
 void setup()
 {
   for (int thisNote = 0; thisNote < 8; thisNote++)
   {
-    // to calculate the note duration, take one second divided by the note type.
-    // e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    // to calculate duration, divide one second with note type, e.g. quarter = 1000/4, eighth = 1000/8
     int noteDuration = 1000 / noteDurations[thisNote];
     tone(8, melody[thisNote], noteDuration);
 
-    // to distinguish the notes, set a minimum time between them.
-    // the note's duration + 30% seems to work well:
+    // to distinguish the notes, set small time between them.
     int pauseBetweenNotes = noteDuration * 1.30;
     delay(pauseBetweenNotes);
 
-    // stop the tone playing:
+    // stop tone playing
     noTone(8);
   }
 }
