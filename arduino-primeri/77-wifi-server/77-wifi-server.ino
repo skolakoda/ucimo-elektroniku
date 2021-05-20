000422913efa1df0693c38ee17b7119123495e60
Damjan Pavlica <damjan.pavlica@origo.is>
2021-05-20 18:19:26 +0200

    cistka

--- arduino-primeri/77-wifi-server/77-wifi-server.ino
+++ arduino-primeri/77-wifi-server/77-wifi-server.ino
@@ -8,63 +8,36 @@ const char* password = "skolakoda523";
 
 ESP8266WebServer server(80);
 
-const int led = 13;
-
 void handleRoot() {
-  digitalWrite(led, 1);
-  server.send(200, "text/plain", "hello from esp8266!");
-  digitalWrite(led, 0);
+  server.send(200, "text/plain", "zdravo svete!");
 }
 
 void handleNotFound() {
-  digitalWrite(led, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += server.method();
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
+  server.send(404, "text/plain", "stranica ne postoji\n");
 }
 
 void setup(void) {
-  pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
-  // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
+  Serial.print("server radi na: ");
   Serial.println(WiFi.localIP());
-
-  if (MDNS.begin("esp8266")) {
-    Serial.println("MDNS responder started");
-  }
 
   server.on("/", handleRoot);
 
-  server.on("/inline", []() {
-    server.send(200, "text/plain", "this works as well");
+  server.on("/prd", []() {
+    server.send(200, "text/plain", "ova stranica radi");
   });
 
   server.onNotFound(handleNotFound);
 
   server.begin();
-  Serial.println("HTTP server started");
 }
 
 void loop(void) {
