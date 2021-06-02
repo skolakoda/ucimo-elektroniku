20fc694287a0b6e52dc4f798d161f7cde22a6358
Damjan Pavlica <damjan.pavlica@origo.is>
2021-06-02 16:55:19 +0200

    vs code arduino podesavanja

--- arduino-primeri/58-fotootpornik/58-fotootpornik.ino
+++ arduino-primeri/58-fotootpornik/58-fotootpornik.ino
@@ -1,11 +1,13 @@
 // https://create.arduino.cc/projecthub/MisterBotBreak/how-to-use-a-photoresistor-46c5eb
+// objasnjenje: https://techexplorations.com/guides/arduino/common-circuits/voltage-divider-photoresistor/
+int fotootpornik = A0;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  int value = analogRead(A0);
+  int value = analogRead(fotootpornik);
   Serial.println("Analog value : ");
   Serial.println(value);
   delay(250);
