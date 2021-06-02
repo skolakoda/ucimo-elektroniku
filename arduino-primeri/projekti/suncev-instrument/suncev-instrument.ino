2e24ddfecfbbc290a471a30a091432e014b69a85
Damjan Pavlica <damjan.pavlica@origo.is>
2021-06-02 17:13:40 +0200

    pomalo

--- arduino-primeri/projekti/suncev-instrument/suncev-instrument.ino
+++ arduino-primeri/projekti/suncev-instrument/suncev-instrument.ino
@@ -1,19 +1,21 @@
 int fotootpornik = A0;
 int zvucnik = 8; // plus zvučnika
 
+int granicaTame = 20;
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
   int svetlost = analogRead(fotootpornik);
-  Serial.print("fotootpornik : ");
-  Serial.println(svetlost);
   // TODO: napraviti trajanje promenljivim
   unsigned long trajanje = 1000 / 4;
-
-  unsigned int visina = map(svetlost, 0, 1023, 31, 4978);
-  tone(zvucnik, visina, trajanje);
-  
+  Serial.print("fotootpornik : ");
+  Serial.println(svetlost);
   delay(trajanje);
+
+  if (svetlost < granicaTame) return;
+  unsigned int visina = map(svetlost, granicaTame, 1023, 31, 4978);
+  tone(zvucnik, visina, trajanje);
 }
