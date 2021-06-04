ced27eaccbbf5db45b178de5d938c1b81c9aa28f
Damjan Pavlica <damjan.pavlica@origo.is>
2021-06-04 14:47:59 +0200

    m

--- arduino-primeri/projekti/suncev-instrument/suncev-instrument.ino
+++ arduino-primeri/projekti/suncev-instrument/suncev-instrument.ino
@@ -1,17 +1,12 @@
 byte fotootpornik = A0;
 byte zvucnik = 5; // plus zvučnika
-int echoPin = 7;
-int trigPin = 8;
 
-byte granicaTame = 20;
+byte granicaTame = 60;
 byte najdubljiZvuk = 31;
 int najvisiZvuk = 4978;
-long duration, cm;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
 }
 
 void loop() {
